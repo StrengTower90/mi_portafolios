afb8bbe224037bd4523dd890ac74d24632932afd
Luis Escalante <luis.escalante.c0590@outlook.com>
2025-06-18 22:16:13 -0600

    still having the same styles

--- src/components/skills/SvgIcon.jsx
+++ src/components/skills/SvgIcon.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./Skills.css";
 
-const SvgIcon = ({ src, title, level }) => {
+const SvgIcon = ({ src, title, level, review }) => {
     return (
         <div className="skill___item">
-            {src ? (
+          <div className="skill__img">
+              {src ? (
                 <img src={src} alt={title} />
             ) : (
                 <h4>No SVG Available</h4>
             )}
+          </div>
+          <div className="skill__review">
             <span>{title}</span>
-            <div className="progress__bar">
+            <p>{review}</p>
+
+          </div>
+            {/* <div className="progress__bar">
                 <div className="progress__fill" style={{ width: `${level}%`}}>
                     <span>{level}%</span>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 };
